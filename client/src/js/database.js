9a8bf7311871b35ba1ca10d4e2ca82060ce777c0
danyon talbot <danyon.talbot2@gmail.com>
2024-01-09 21:37:11 +1100

    Added Method for Adding Notes to Database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const transaction = db.transaction('jate', 'readwrite');
+    const noteStore = transaction.objectStore('notes');
+    await noteStore.add(note);
+    console.log('Note Added');
+  } catch (error) {
+    console.error('Error Adding Notes', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
